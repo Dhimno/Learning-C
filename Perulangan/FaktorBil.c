ffeff6ef96b21521360cc02e612dcb5bc6a4820e
Dhimno <tylerpappas91@gmail.com>
2025-03-04 11:39:56 +0700

    a

---
+++ Perulangan/FaktorBil.c
@@ -0,0 +1,31 @@
+/* Nama File 	: JumDeret.c */
+/* Deskripsi 	: Menghitung dan menampilkan jumlah total deret bilangan ke-N */
+/* Pembuat   	: <24060124120010 - Dhimas Reza Nafi Wahyudi> */
+/* Tgl Pembuatan	: <04-03-2025 dan 08:39> */
+
+#include <stdio.h> /* Header file utama */
+
+/* Program Utama */
+int main() {
+    /* Kamus */
+    int n; 
+    int i = 1; 
+    int sum = 0; 
+
+/* Algoritma */
+    printf("========================================\n");
+    printf("Penghitung jumlah total deret ke-N\n");
+    printf("========================================\n");
+    printf("Masukkan nilai N: ");
+    scanf("%d", &n);
+
+    while (i <= n) { 
+        sum += i;    
+        i++;
+    }
+
+    printf("Jumlah total deret ke-%d adalah: %d\n", n, sum);
+
+    return 0;
+}
+
